6da4a5eb312c444b6d985cbced08569db25e3c55
iconojdiazch <khepherer@gmail.com>
2017-01-08 12:22:48 +0100

    Futuros y archivos

---
+++ ejemplos/EjemploArchivos/src/main/scala/Principal.scala
@@ -0,0 +1,24 @@
+import scala.io.Source
+
+object Principal extends App {
+  List(enSecuencia)
+    .map(tiempo(_))
+    .foreach {
+      case (x, y) => println(s"Tiempo: $x. Palabras: $y")
+    }
+
+  def enSecuencia = {
+    val holmesUrl = """http://www.ericrochester.com/clj-data-analysis/data/pg1661.txt"""
+    val data = Source fromURL (holmesUrl) getLines
+    import scala.collection.mutable
+    val contador = mutable.Map[String, Int]().withDefault(_ => 0)
+    for (clave <- data.flatMap(_.split("\\s+")))
+      contador(clave) += 1
+    contador.values.sum
+  }
+
+  def tiempo(bloque: => Int) = {
+    val inicio = System.nanoTime
+    (System.nanoTime - inicio, bloque)
+  }
+}
--- ejemplos/EjemploFuturos/src/main/scala/sistema/Principal5.scala
+++ ejemplos/EjemploFuturos/src/main/scala/sistema/Principal5.scala
@@ -1,28 +1,18 @@
 package sistema
+
+import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.io.Source
-import scala.concurrent.duration._
 import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
+import scala.io.Source
 
 object Principal5 extends App {
-  List(enSecuencia)
-    .map(tiempo(_))
-    .foreach {
-      case (x, y) => println(s"Tiempo: $x. Palabras: $y")
-    }
-
-  def enSecuencia = {
-    val holmesUrl = """http://www.ericrochester.com/clj-data-analysis/data/pg1661.txt"""
-    val data = Source fromURL (holmesUrl) getLines
-    import scala.collection.mutable
-    val contador = mutable.Map[String, Int]().withDefault(_ => 0)
-    for (clave <- data.flatMap(_.split("\\s+")))
-      contador(clave) += 1
-    contador.values.sum
-  }
-
-  def tiempo(bloque: => Int) = {
-    val inicio = System.nanoTime
-    (System.nanoTime - inicio, bloque)
-  }
-}
+  val holmesUrl = """http://www.ericrochester.com/clj-data-analysis/data/pg1661.txt"""
+  val textos = Future(for {
+    archivo <- Seq(holmesUrl)
+    origen <- Source fromURL (archivo) getLines
+  } yield origen)
+  val totalPalabras = textos.flatMap(
+    seq => Future { seq map(_.split("\\s+").length) }) map(_.sum)
+  println(s"Total palabras: ${Await.result(totalPalabras, 1 minute)}")
+} 
