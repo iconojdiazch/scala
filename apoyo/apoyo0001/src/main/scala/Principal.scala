5684adb6e8f2f5cdc7c13973e509ea5f9f25ac87
iconojdiazch <khepherer@gmail.com>
2017-01-10 20:37:50 +0100

    apoyo0001 actualizado

--- apoyo/apoyo0001/src/main/scala/Principal.scala
+++ apoyo/apoyo0001/src/main/scala/Principal.scala
@@ -4,13 +4,17 @@ import apoyo0001.PersonaScala
 object Principal extends App {
   val personaJava = new Persona("a", "b")
   println(personaJava.getNombre())
-  
+
   val personaScala = new PersonaScala("c", "d")
+
   println(personaScala.nombre)
-  
+
   val s = "Inferencia de tipos"
   println(s)
-  
+
   val funcion: String => String = s => s.toUpperCase()
   println(funcion("uno"))
+
+  def funcionUsandoDef: Int => String = _.toString  
+  println(s"Resultado: ${funcionUsandoDef(100)}")
 }
